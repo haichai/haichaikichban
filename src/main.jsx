1d3e422d9444bdee30f67c414631d9a1b871fe48
haichai <quang.vuthanh@starspirits.vn>
2026-07-09 09:43:06 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,1 @@
-export default {
-  async fetch(request, env, ctx) {
-    const destinationURL = "https://haichai-script-studio.web.app/";
-    return Response.redirect(destinationURL, 301);
-  },
-};
+window.location.replace("https://haichai-script-studio.web.app/");
